95743b8e008339b7087a95fc49d46521624f336b
Debajyoti Halder <rondebajyoti@gmail.com>
2022-05-18 00:14:29 +0530

    initial boilerplate commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
